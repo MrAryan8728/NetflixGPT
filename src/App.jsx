e53798e119519ec076a4bcdef055918785c830ee
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-07-04 19:53:22 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className=' font-bold text-2xl text-green-600'>Aryan is here with tailwind and React</h1>
+    </>
+  )
+}
+
+export default App
